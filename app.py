53ddb38451116f7c05a70525a237704b401dddb0
Brooks Walsh <lbw223@lehigh.edu>
2023-04-15 11:50:46 -0400

    working select box for zone_type

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ st.set_page_config(
 with st.sidebar:
     
     "Month Selector Test"
-    st.selectbox("Month", ["3-2023",'4-2023','5-2023'])
+    mont_select = st.selectbox("Month", ["3-2023",'4-2023','5-2023'])
+    
+    "Geography Type Selector Test"
+    zone_type = st.selectbox("Zone Type", ['Neighborhoods','Census-Tracts'])
 
     # % chance lose, $ lose, % chance win, $win, CARA formula e, CARA formula V
     qs ={1 :[.50,0,.50,10   ],
@@ -327,10 +330,12 @@ st.plotly_chart(fig5,use_container_width=True)
 - If your leverage is more than 1 and your risk aversion low enough, the optimal portfolio might involve borrowing money to invest in equities; if so, the red star will be to the right of the blue star
 '''
 
-########################################################
+######################################################
 # start of my contributions
 ######################################################
 
+######################################################
+# redundant note to help me notice where my test starts
 
 #######################################################################
 # Test: making a working display with our map
@@ -353,46 +358,74 @@ listings = gpd.GeoDataFrame(listings, geometry=listings.apply(
         lambda srs: shapely.geometry.Point(srs['longitude'], srs['latitude']), axis='columns'
     ))
 
-# load map of neighborhoods
-boston_NBH = gpd.read_file("inputs/Census2020_BG_Neighborhoods/Census2020_BG_Neighborhoods.shp")
+# the following if/else uses zone_type selectbox
+if zone_type == "Neighborhoods":
+    # load map of neighborhoods
+    boston_NBH = gpd.read_file("inputs/Census2020_BG_Neighborhoods/Census2020_BG_Neighborhoods.shp")
 
-# gotta make same fix for CRS as above
-boston_NBH = boston_NBH.to_crs('epsg:4326')
+    # fix for non-stardard coordinates, see notebook for details
+    boston_NBH = boston_NBH.to_crs('epsg:4326')
 
-# function to create a new column based on whether or not a listing is in a neighborhood
-def assign_census_NBH(bnb):
-    bools = [geom.contains(bnb['geometry']) for geom in boston_NBH['geometry']]
-    if True in bools:
-        return boston_NBH.iloc[bools.index(True)]['BlockGr202']
-    else:
-        return np.nan
+    # function to create a new column based on whether or not a listing is in a neighborhood
+    def assign_census_NBH(bnb):
+        bools = [geom.contains(bnb['geometry']) for geom in boston_NBH['geometry']]
+        if True in bools:
+            return boston_NBH.iloc[bools.index(True)]['BlockGr202']
+        else:
+            return np.nan
 
+    # .apply the function to the listings
+    listings['census_NBH'] = listings.apply(assign_census_NBH, axis='columns')
 
-# .apply the function to the listings
-listings['census_NBH'] = listings.apply(assign_census_NBH, axis='columns')
+    # use .map() to apply value_counts to each value of 'BlockGr202'
+    boston_NBH['BNBs'] = boston_NBH['BlockGr202'].map(listings['census_NBH'].value_counts())
+    boston_NBH['BNBs'] = boston_NBH['BNBs'].fillna(0)
+    boston_NBH.set_index('BlockGr202', inplace=True)
 
-# use .map() to apply value_counts to each value of 'BlockGr202'
-boston_NBH['BNBs'] = boston_NBH['BlockGr202'].map(listings['census_NBH'].value_counts())
-boston_NBH['BNBs'] = boston_NBH['BNBs'].fillna(0)
-boston_NBH.set_index('BlockGr202', inplace=True)
+    # this code reprojects the areas into an "equal-area" projection
+    # this is so that I can get listings per Kilometer^2
+    boston_NBH['BNBDensity'] = (boston_NBH['BNBs'] / boston_NBH['geometry']\
+                                .to_crs('epsg:3395')\
+                                .map(lambda p: p.area / 10**6))\
+                                .fillna(0)
+    fig = px.choropleth(boston_NBH, geojson=boston_NBH.geometry, locations=boston_NBH.index,
+                        color="BNBDensity", hover_data=['BNBs'])
+    fig.update_geos(fitbounds="locations", visible=False)
 
-# this code reprojects the areas into an "equal-area" projection
-# this is so that I can get listings per Kilometer^2
-boston_NBH['BNBDensity'] = (boston_NBH['BNBs'] / boston_NBH['geometry']\
-                            .to_crs('epsg:3395')\
-                            .map(lambda p: p.area / 10**6))\
-                            .fillna(0)
+#####################################################
+# load map of census tracts (same steps as above for NBH)
+
+# else implies census tract data
+else:
+    boston_tract = gpd.read_file("inputs/Census2020_Tracts/Census2020_Tracts.shp")
+    boston_tract = boston_tract.to_crs('epsg:4326')
+
+    def assign_census_tract(bnb):
+        bools = [geom.contains(bnb['geometry']) for geom in boston_tract['geometry']]
+        if True in bools:
+            return boston_tract.iloc[bools.index(True)]['NAME20']
+        else:
+            return np.nan
+
+    listings['census_tract'] = listings.apply(assign_census_tract, axis='columns')
+    boston_tract['BNBs'] = boston_tract['NAME20'].map(listings['census_tract'].value_counts())
+    boston_tract['BNBs'] = boston_tract['BNBs'].fillna(0)
+    boston_tract.set_index('NAME20', inplace=True)
+    boston_tract['BNBDensity'] = (boston_tract['BNBs'] / boston_tract['geometry']\
+                                .to_crs('epsg:3395')\
+                                .map(lambda p: p.area / 10**6))\
+                                .fillna(0)
+    fig = px.choropleth(boston_tract, geojson=boston_tract.geometry, locations=boston_tract.index,
+                        color="BNBDensity", hover_data=['BNBs'])
+    fig.update_geos(fitbounds="locations", visible=False)
 
 #####################################################
 # Create a fake data set that includes desired variables
 # then merge it with boston_NBH
+# allow user to select desired month
+#      - so we need data per month
 # this will suffice for proposal
 #####################################################
 
-
-# neighborhood choropleth map
-fig = px.choropleth(boston_NBH, geojson=boston_NBH.geometry, locations=boston_NBH.index,
-                    color="BNBDensity", hover_data=['BNBs'])
-fig.update_geos(fitbounds="locations", visible=False)
-
+# displaying plot
 st.plotly_chart(fig,use_container_width=False)
