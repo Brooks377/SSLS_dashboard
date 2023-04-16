618b2de424463c3f9a7b49bfea712e29523e03cf
Brooks Walsh <lbw223@lehigh.edu>
2023-04-15 22:42:01 -0400

    complete proposal dashboard

--- app.py
+++ app.py
@@ -28,18 +28,29 @@ st.set_page_config(
 # Boston Airbnb Pricing Analysis/Prediction 
 """
 
+# load all data first, some is needed for sidebar
+boston_NBH = gpd.read_file("inputs/Census2020_BG_Neighborhoods/Census2020_BG_Neighborhoods.shp")
+listings = pd.read_csv('inputs/listings.csv.gz', compression='gzip')
+boston_tract = gpd.read_file("inputs/Census2020_Tracts/Census2020_Tracts.shp")
+
 #############################################
 # start: sidebar
 #############################################
 
 with st.sidebar:
     
-    "Month Selector Test"
-    mont_select = st.selectbox("Month", ["3-2023",'4-2023','5-2023'])
+    "Select Month"
+    month_select = st.selectbox("Month", ["3-2023",'4-2023','5-2023'])
     
-    "Geography Type Selector Test"
+    "Select Zone Type"
     zone_type = st.selectbox("Zone Type", ['Neighborhoods','Census-Tracts'])
     
+    if zone_type == 'Neighborhoods':
+        "Select Neighborhood"
+        zone_select = st.selectbox("Neighborhood", boston_NBH['BlockGr202'].tolist())
+    else:
+        "Select Census Tract"
+        zone_select = st.selectbox("Census Tract", boston_tract['NAME20'].tolist())
     '''
     [Source code and contributors here.](https://github.com/donbowen/portfolio-frontier-streamlit-dashboard)
     '''
@@ -60,8 +71,6 @@ with st.sidebar:
 
 # copied from 9th_grade_geography_test
 
-listings = pd.read_csv('inputs/listings.csv.gz', compression='gzip')
-
 # this step maps each longitude and latitude to a shapely point
 listings = gpd.GeoDataFrame(listings, geometry=listings.apply(
         lambda srs: shapely.geometry.Point(srs['longitude'], srs['latitude']), axis='columns'
@@ -69,9 +78,6 @@ listings = gpd.GeoDataFrame(listings, geometry=listings.apply(
 
 # the following if/else uses zone_type selectbox
 if zone_type == "Neighborhoods":
-    # load map of neighborhoods
-    boston_NBH = gpd.read_file("inputs/Census2020_BG_Neighborhoods/Census2020_BG_Neighborhoods.shp")
-
     # fix for non-stardard coordinates, see notebook for details
     boston_NBH = boston_NBH.to_crs('epsg:4326')
 
@@ -106,7 +112,6 @@ if zone_type == "Neighborhoods":
 
 # else implies census tract data
 else:
-    boston_tract = gpd.read_file("inputs/Census2020_Tracts/Census2020_Tracts.shp")
     boston_tract = boston_tract.to_crs('epsg:4326')
 
     def assign_census_tract(bnb):
@@ -128,6 +133,9 @@ else:
                         color="BNBDensity", hover_data=['BNBs'])
     fig.update_geos(fitbounds="locations", visible=False)
 
+# displaying plot
+st.plotly_chart(fig,use_container_width=False)
+
 #####################################################
 # Create a fake data set that includes desired variables
 # then merge it with boston_NBH
@@ -136,5 +144,90 @@ else:
 # this will suffice for proposal
 #####################################################
 
-# displaying plot
-st.plotly_chart(fig,use_container_width=False)
+######################################################
+# stats about map
+######################################################
+
+"""
+## This section will show stats about the selected neighborhood/tract.
+    - Changes based on sidebar inputs
+"""
+if zone_type == 'Neighborhoods':
+    example_output = boston_NBH.loc[zone_select,'BNBs']
+    example_output2 = boston_NBH.loc[zone_select,'Shape_Leng']
+    f'Example Output (# BNBs): {example_output}'
+    f'Example Output 2 (Perimeter length): {example_output2}'
+else:
+    example_output = boston_tract.loc[zone_select,'BNBs']
+    example_output2 = boston_tract.loc[zone_select,'Shape_STLe']
+    f'Example Output (# BNBs): {example_output}'
+    f'Example Output 2 (Perimeter length): {example_output2}'
+
+######################################################
+#: Report Section
+######################################################
+
+"""
+## This section will show the report file as a markdown document
+    - Below is our initial proposal, to ensure markdown files display properly
+
+# Research Proposal: Analysis of Boston Airbnb Listings
+### Big Picture Question: What attributes about a Boston Airbnb listing are most influential to its list price?
+
+By Tommy McDade, Brooks Walsh, and Taylor Sheridan
+
+## Research Question
+
+After finding correlations and testing different influences, can we predict future Airbnb list prices?
+   - This project will be about relationships as well as prediction. 
+       - We will do a thorough EDA on our data set (including visuals) to represent the many relationships between different listing attributes and price.
+       - After the important relationships are determined, we will use regression models and machine learning to create an accurate predictor of list prices
+       - The metrics for success in this project include:
+           - A dashboard to visually represent different metrics based on geographic location
+           - A maximized R-Squared value (*or a different maximized/minimized measurement statistic, based on our choice of model*)
+
+## Necessary Data
+
+1. The Data will be primarily collected from [kaggle](https://www.kaggle.com/datasets/airbnb/boston):
+- sample period: 9/5/2016 - 9/4/2017
+    - There are 3 data sets included from Kaggle:
+    1. calendar.csv
+        - observation: Airbnb listings
+        - rows: 1.31 million
+        - columns: 4
+        - index: "listing_id" and "date"
+        - variables: "available (Bool)", "price(continuous)"
+            - price is missing 51% of values
+    2. listings.csv
+        - observation: Airbnb listings
+        - rows: 3585
+        - columns: 95
+        - index: "id", "scrape_id"
+        - variables: too many to reasonably include in a proposal
+            - a few examples:
+                - "host_since", "maximum_nights", "require_guest_profile_picture"
+    3. reviews.csv
+        - observation: reviews based on Airbnb listings
+        - rows: 68.3k
+        - columns: 6
+        - index: "listing_id", "id", "reviewer_id"
+        - variables: "date", "reviewer_name", "comments"
+
+2. To display maps and geographic data, we will need [2020 census tract data](https://data.boston.gov/dataset/census-2020-tracts/resource/1721fbb7-ee56-4a61-9e1b-2f2342d202d1).
+    - We will likely be using the shapefile version    
+
+3. The **raw inputs** for this project will be 3 of the 4 data sets described above in .csv format and a shapefile for use in geographic data.
+    - All of these data sets will be saved to a folder called "inputs"
+    - Any important dataframes/visuals that are created will be saved to a folder called "outputs"
+
+4. High-level data cleaning plan:
+- The 3 datasets that are obtained from Kaggle (calendar.csv, listings.csv, reviews.csv) are already fairly clean
+    - There are several variables that may need to be a different data type in order to perform analysis
+    - Some variables are full of text, and will likely need parsing to be used effectively
+    - Some variables are URLs, which likely aren't needed
+    - Because there are so many variables, and some are quite specific:
+        - There are **a LOT** of NAN values that should be replaced with 0 (or a value that is consistent with the context)
+- The last data set (used to create the map) is from 2020 census, and is not in a usable format originally
+    - luckily there is a package for python that reads shapefiles effectively:
+        - ```import geopandas as gpd```
+"""
