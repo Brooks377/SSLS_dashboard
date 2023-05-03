c7bddf2d8b8a153dc77c325add53ce2c6f5f2689
Thomas McDade <tjm224@lehigh.edu>
2023-05-03 00:23:49 -0400

    Trying something

--- Overview.py
+++ Overview.py
@@ -51,8 +51,8 @@ with st.sidebar:
 #####################################################
 
 if page_select == 'Project Overview':
-    """
-    ## Project Purpose
+    st.markdown( 
+    "## Project Purpose
     Since its inception in 2008, Airbnb has provided travelers with an alternative to staying in hotels or traditional bed and breakfasts. Airbnb has also allowed property owners to gain extra revenue by listing their properties for short or long term. Airbnb properties can range from luxury apartments to quaint places to crash. As a result, there is a large income range for potential renters and property owners. The diversity of properties allows for a variety in property types, locations, and amenities. 
 
     Due to the variety in properties an aspiring host may not know how to exactly price their property. A property owner may not know which amenities will result in the maximum value for their property. Additionally, investors may look to acquire properties to rent as long term Airbnbs, to generate the maximum value. 
@@ -114,26 +114,26 @@ if page_select == 'Project Overview':
     # **About Us:**
 
     #### Brooks Walsh
-    ![Brooks](inputs/Brooks.png "Logo Title Text 2")
+    ![Brooks](inputs/Brooks.png)
     - Major: Finance
     - Year of Graduation: 2024
     - Interests/Hobbies:  Hiking, soccer, reading
 
 
     #### Taylor Sheridan
-    ![Taylor](inputs/Taylor.png "Logo Title Text 2")
+    ![Taylor](inputs/Taylor.png)
     - Major: Finance & Business Analytics
     - Year of Graduation: 2024
     - Interests/Hobbies: Golf, working out, visiting the beach
 
     #### Thomas McDade
-    ![Tommy](inputs/Tommy.png "Logo Title Text 2")
+    ![Tommy](inputs/Tommy.png)
     - Major: Finance & Business Analytics
     - Year of Graduation: 2024
-    - Interests/Hobbies: Sports betting, EDM music, nature
+    - Interests/Hobbies: Sports betting, EDM music, nature")
 
 
-    """
+  
 elif page_select == 'EDA & Methodology':
     with open('inputs/EDA_Methodology.html', 'r', encoding='utf-8') as f:
         html_file = f.read()
